848f35a11b1496e1a9146d45170522d29b17ec60
Jessica Wilkins <jwilkin4@hotmail.com>
2021-12-11 10:53:20 -0800

    adding prev and next buttons for pagination - still needs work

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import UserCards from './components/UserCards';
 
 const App = () => {
@@ -8,35 +8,51 @@ const App = () => {
   const [userLocation, setUserLocation] = useState(null);
   const [followersArr, setFollowersArr] = useState(null);
   const [followingArr, setFollowingArr] = useState(null);
+  const [currPage, setCurrPage] = useState(1);
 
-
-  const displayData = (input) => {
-    if (input === '') {
-      return
-    }
-    return fetch(`https://api.github.com/search/users?q=${input}`)
-      .then(res => res.json())
-      .then((user) => {
-        setUsers(user)
-        Promise.all(
-          user.items.map(
-            profile => fetch(profile.url)
-              .then(res => res.json())
-          )
-        ).then(url => {
-          const bios = url.map(profile => profile.bio)
-          const locations = url.map(profile => profile.location)
-          const followers = url.map(profile => profile.followers)
-          const following = url.map(profile => profile.following)
-          setBioArr(bios)
-          setUserLocation(locations)
-          setFollowersArr(followers)
-          setFollowingArr(following)
-          console.log(bios)
-        })
-      })
+  const prevPage = () => {
+    setCurrPage(curr => curr - 1)
+    displayData()
   }
 
+  const nextPage = () => {
+    setCurrPage(curr => curr + 1)
+    displayData()
+  }
+
+  const resetPage = () => {
+    setCurrPage(curr => curr = 1)
+    displayData()
+  }
+
+  useEffect(() => {
+    console.log(currPage)
+  }, [currPage])
+
+  const displayData = async () => {
+    window.scrollTo(0, 0)
+    if (userInput === '') {
+      return
+    }
+    const res = await fetch(`https://api.github.com/search/users?q=${userInput}&per_page=16&page=${currPage}`);
+    const user = await res.json();
+    setUsers(user);
+    Promise.all(
+      user.items.map(
+        profile => fetch(profile.url)
+          .then(res_1 => res_1.json())
+      )
+    ).then(url => {
+      const bios = url.map(profile_1 => profile_1.bio);
+      const locations = url.map(profile_2 => profile_2.location);
+      const followers = url.map(profile_3 => profile_3.followers);
+      const following = url.map(profile_4 => profile_4.following);
+      setBioArr(bios);
+      setUserLocation(locations);
+      setFollowersArr(followers);
+      setFollowingArr(following);
+    });
+  }
 
 
   const props = {
@@ -51,14 +67,18 @@ const App = () => {
     <>
       <h1>GitHub Search and Learn App</h1>
       <label htmlFor="site-search">Search for any user on GitHub
-        <input onKeyDown={(e) => e.key === 'Enter' ? displayData(userInput) : null} autoComplete='off' onChange={(e) => setUserInput(e.target.value)} value={userInput} type="search" id="site-search" name="q" aria-label="Search through site content" />
+        <input onKeyDown={(e) => e.key === 'Enter' ? displayData() : null} autoComplete='off' onChange={(e) => setUserInput(e.target.value)} value={userInput} type="search" id="site-search" name="q" aria-label="Search through site content" />
       </label>
-      <button onClick={() => displayData(userInput)}>Search</button>
+      <button onClick={resetPage}>Search</button>
 
       {users && userLocation && followersArr && followingArr ?
         <UserCards {...props} /> :
-        <h2>Discover user profiles or login to receive personalized learning plans</h2>
+        <h2>Discover user profiles </h2>
       }
+      <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+        {currPage === 1 ? null : <button onClick={prevPage}>Previous</button>}
+        {!users ? null : <button onClick={nextPage}>Next</button>}
+      </div>
     </>
   );
 }
